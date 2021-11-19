8700c00dca634fda3e8c374a1c7af2d0e23e0c3f
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 21:13:48 +0100

    Fix modulo

--- src/main/java/com/github/micha4w/Teams.java
+++ src/main/java/com/github/micha4w/Teams.java
@@ -92,7 +92,7 @@ public enum Teams {
         final int signZ = Integer.signum(opposite.chunkZ);
 
         Chunk chunk = Teams.world.getChunkAt(16 * chunkX, 16 * chunkZ);
-        Location corner = chunk.getBlock(signX % 16, (int) yLevel, signZ % 16).getLocation();
+        Location corner = chunk.getBlock(signX < 0 ? 15 : 0, (int) yLevel, signZ < 0 ? 15 : 0).getLocation();
 
         return corner.add(distanceToCorner * signX, 0, distanceToCorner * signZ);
     }
